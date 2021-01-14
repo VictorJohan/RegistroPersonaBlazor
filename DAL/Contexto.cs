401d208a720c9685c91a24c895faf34c39d1e2c0
Victor Johan <Vicjohan23@gmail.com>
2021-01-14 16:32:14 -0400

    SE CREO EL CONTEXTO Y SE HIZO LA CONECCION.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPersonaBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPersonaBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+    }
+}
